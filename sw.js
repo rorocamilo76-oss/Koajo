6cf9261c95604ae8b5585076c7ca67e0b9627a95
rorocamilo76-oss <rorocamilo76@gmail.com>
2026-05-09 17:07:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,39 @@
 // KOAJ – Service Worker
-const CACHE = 'koaj-v1';
-const ARCHIVOS = [
-  '/BogotaVerde/',
-  '/BogotaVerde/index.html',
-  '/BogotaVerde/style.css',
-  '/BogotaVerde/script.js',
-  '/BogotaVerde/icon.svg',
-  '/BogotaVerde/manifest.json'
+var CACHE = 'koaj-v2';
+var ARCHIVOS = [
+  '/koaj/',
+  '/koaj/login.html',
+  '/koaj/index.html',
+  '/koaj/style.css',
+  '/koaj/script.js',
+  '/koaj/icon.svg',
+  '/koaj/manifest.json'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ARCHIVOS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(function(res) {
+      return res || fetch(e.request);
+    })
   );
 });
-
